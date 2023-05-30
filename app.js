51a85c356ab6d5d44a0631929cc89354c6d513ad
LackErik <93646851+LackErik@users.noreply.github.com>
2023-05-30 14:11:03 +0200

    Start Seite
    Die Größen stimmen noch nicht

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+//den Ordner Public freigeben
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/style"));
+app.use(express.static(__dirname + "/pictures"));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
