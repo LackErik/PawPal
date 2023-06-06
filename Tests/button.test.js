ca8452d2846eeb1dd55d61f7b89cc0a36bb58990
kjboos <kj.boos@icloud.com>
2023-06-06 10:50:11 +0200

    Headless mode

--- Tests/button.test.js
+++ Tests/button.test.js
@@ -6,7 +6,7 @@ describe('Button Test', () => {
 
   beforeAll(async () => {
     //browser = await puppeteer.launch();
-    browser = await puppeteer.launch({ headless: 'new' });
+    browser = await puppeteer.launch({ headless: true });
     page = await browser.newPage();
     await page.goto('http://localhost:3000'); // Hier die URL der HTML-Seite einfügen
   });
