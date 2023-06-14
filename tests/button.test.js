eb5801f4b3a0eee8fee47e9fc1622cd2b3a4ed7d
kjboos <kj.boos@icloud.com>
2023-06-14 13:46:39 +0200

    HomeButtonTest hinzugefügt

--- tests/button.test.js
+++ tests/button.test.js
@@ -44,6 +44,18 @@ describe('Button Test', () => {
     expect(url).toBe('http://localhost:3000/home'); // Hier die erwartete Weiterleitungs-URL zurück zur Hauptseite einfügen
   });
 
+  it('should simulate a click event on the home button and redirect to the home page', async () => {
+    await page.waitForSelector('#logo'); // Hier den Selektor des Home Buttons angeben
+  
+    const homeButton = await page.$('#logo');
+    await homeButton.click();
+  
+    await page.waitForNavigation({ timeout: 10000 });
+  
+    const url = await page.url();
+    expect(url).toBe('http://localhost:3000/home'); // Hier die erwartete Weiterleitungs-URL zur Startseite einfügen
+  });
+
   it('should simulate a click event on the "Impressum" link and redirect to the impressum page', async () => {
     await page.waitForSelector('#impressum_btn'); // Hier den Selektor des "Impressum" Links angeben
 
